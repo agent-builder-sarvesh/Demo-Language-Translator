9004ca989223da5c6b46290a4d0bfd0dcfd23227
Sarva-gms <sarva@gmobility.com>
2026-02-28 01:35:17 +0530

    adding all laguages

--- app.py
+++ app.py
@@ -5,9 +5,17 @@ from deep_translator import GoogleTranslator
 app = Flask(__name__)
 datetoday2 = date.today().strftime("%d-%B-%Y")
 
+# Get all supported languages automatically
+languages = GoogleTranslator().get_supported_languages(as_dict=True)
+
 @app.route('/')
 def home():
-    return render_template('home.html', datetoday2=datetoday2, res='')
+    return render_template(
+        'home.html',
+        datetoday2=datetoday2,
+        res='',
+        languages=languages
+    )
 
 @app.route('/translate', methods=['POST'])
 def translate():
@@ -19,7 +27,12 @@ def translate():
     except Exception as e:
         result = f"Error: {e}"
 
-    return render_template('home.html', datetoday2=datetoday2, res=result)
+    return render_template(
+        'home.html',
+        datetoday2=datetoday2,
+        res=result,
+        languages=languages
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, port=5900)
